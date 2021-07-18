5a5304d02c5d51bd3a1a3465bdddc978eb90642a
John Rees <john@bitsushi.com>
2021-07-18 15:01:42 +0100

    render rust... kinda

--- src/components/Rust.tsx
+++ src/components/Rust.tsx
@@ -4,7 +4,7 @@ const Rust = ({ rust }: { rust: any }) => (
   <div
     style={{ background: "black", padding: 20, color: "white", height: "100%" }}
   >
-    <pre>{JSON.stringify(rust, null, 2)}</pre>
+    <pre>{rust}</pre>
   </div>
 );
 
--- src/lib/parse.ts
+++ src/lib/parse.ts
@@ -1,38 +1,60 @@
+import camelCase from "lodash/camelCase";
+import capitalize from "lodash/capitalize";
+import snakeCase from "lodash/snakeCase";
 import { Project } from "ts-morph";
 
+const pascalCase = (str: string) => capitalize(camelCase(str));
+
 self.onmessage = (e) => {
-  self.postMessage(parse(e.data));
+  self.postMessage(rustify(parse(e.data)));
 };
 
-const parse = (data: string): any => {
+interface Program {
+  name?: string;
+  accounts: Record<string, any>;
+  instructions: Record<string, Record<string, string>>;
+}
+
+const rustify = (program: Program): string => {
+  return [
+    "use anchor_lang::prelude::*;",
+    "#[program]",
+    `mod ${snakeCase(program.name)} {`,
+    `use super::*;`,
+    `}`,
+    ...Object.entries(program.accounts).flatMap(([k]) => {
+      return [
+        `#[account]`,
+        `pub struct ${pascalCase(k)} {`,
+        // `pub data: u64,`,
+        `}`,
+      ];
+    }),
+  ].join("\n");
+};
+
+const parse = (data: string): Program => {
   const project = new Project({ useInMemoryFileSystem: true });
-  project.createSourceFile("p.ts", data);
-  // project.addSourceFileAtPath("p.ts");
+  project.createSourceFile("program.ts", data);
 
-  interface Prog {
-    name?: string;
-    accounts: Record<string, any>;
-    instructions: Record<string, Record<string, string>>;
-  }
-
-  const Program: Prog = {
+  const program: Program = {
     accounts: {},
     instructions: {},
   };
 
-  const sourceFile = project.getSourceFileOrThrow("p.ts");
+  const sourceFile = project.getSourceFileOrThrow("program.ts");
   sourceFile.getClasses().forEach((klass) => {
-    Program.name = klass.getName();
+    program.name = klass.getName();
 
     klass.getProperties().forEach((prop) => {
-      Program.accounts[prop.getName()] = {};
+      program.accounts[prop.getName()] = {};
     });
 
     klass.getMethods().forEach((instruction) => {
-      Program.instructions[instruction.getName()] ??= {};
+      program.instructions[instruction.getName()] ??= {};
 
       instruction.getParameters().forEach((param) => {
-        Program.instructions[instruction.getName()][param.getName()] = param
+        program.instructions[instruction.getName()][param.getName()] = param
           .getType()
           .getText()
           .replace(/[^a-z0-9]/gi, "");
@@ -40,7 +62,7 @@ const parse = (data: string): any => {
     });
   });
 
-  return Program;
+  return program;
 };
 
 // export default parse;
