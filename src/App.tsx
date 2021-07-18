6e9003186b9fac52b8cd8792ba013ebbd7821788
John Rees <john@bitsushi.com>
2021-07-18 16:09:49 +0100

    window sizing

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,10 @@ const App = () => {
 
   return (
     <div style={{ display: "flex", height: "100vh", overflow: "hidden" }}>
-      <div style={{ flex: 1 }}>
+      <div style={{ flex: 3 }}>
         <TypeScript setRust={setRust} />
       </div>
-      <div style={{ flex: 1 }}>
+      <div style={{ flex: 5 }}>
         <Rust rust={rust} />
       </div>
     </div>
