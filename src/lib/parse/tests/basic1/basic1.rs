ab1a7b7e4225e75ca786c21624c0d32b629b4826
John Rees <john@bitsushi.com>
2021-07-19 11:21:23 +0100

    don't use let my_account yet

--- src/lib/parse/tests/basic1/basic1.rs
+++ src/lib/parse/tests/basic1/basic1.rs
@@ -5,14 +5,12 @@ mod basic_1 {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>, data: u64) -> ProgramResult {
-        let my_account = &mut ctx.accounts.my_account;
-        my_account.data = data;
+        ctx.accounts.my_account.data = data;
         Ok(())
     }
 
     pub fn update(ctx: Context<Update>, data: u64) -> ProgramResult {
-        let my_account = &mut ctx.accounts.my_account;
-        my_account.data = data;
+        ctx.accounts.my_account.data = data;
         Ok(())
     }
 }
